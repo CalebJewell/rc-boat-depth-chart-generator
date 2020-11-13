d2d5f571d1b43014c5b8262833c8a9c97b052732
Caleb Stephen Jewell <caleb.jewell@maine.edu>
2020-11-13 16:54:13 -0500

    This program now prints latitude and longitude to an LCD displat that updates every 1500ms. Also added .h and .c files to organize the code.

--- atmel-dev/main.c
+++ atmel-dev/main.c
@@ -13,88 +13,129 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
+#include <string.h>
 
 /* USEFUL DEFINES */
-#define F_CPU				8000000UL //MCU operates at 8MHz
+#define F_CPU					8000000 //MCU operates at 8MHz
 #define USART_BAUDRATE			9600
-#define UBRR0_VALUE			((F_CPU/16/USART_BAUDRATE)-1)
+#define UBRR0_VALUE				(((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 #define DataRegisters			PORTA
 #define DataDirRegisters		DDRA
 #define DataDirControl			DDRD
 #define ControlRegisters		PORTD
-#define en				PORTD5
-#define rs				PORTD6
-#define rw				PORTD7
-#define BUFFER_SIZE			64
+#define en						PORTD5
+#define rs						PORTD6
+#define rw						PORTD7
+#define BUFFER_SIZE				83
 
 /* FUNCTION PROTOTYPES */
 void Commands(unsigned char);
-void sendCharacter(char);
+void sendCharacter(unsigned char);
 void blink(void);
 void init_lcd(void);
-void sendString(char *);
+void sendString(unsigned char *);
 char getChar(void);
 char nextChar(void);
 unsigned char USART_Receive(void);
 void USART_Init(void);
 void USART_Transmit(unsigned char data);
 void USART_Flush(void);
+bool check_gpgga(char *);
+void get_latitude(char *);
+void get_logitude(char *);
 
-/* GLOBAL VARIABLES */
-char data_buffer[BUFFER_SIZE];
-uint8_t read_position = 0;
-bool stop_flag = false;
+int main(void) {
+	
+	unsigned char latitude[16];
+	unsigned char longitude[16];
+	int comma = 0;
+	uint8_t lat_post = 0, long_post = 0;
+	
+	unsigned char ch;
+	unsigned char *gpgga = "$GPGGA,";
+	unsigned char test_buffer[7];
+	test_buffer[6] = '\0';
 
-
-int main(void)
-{		
 	/* Set data direction registers for what I need */
 	DataDirRegisters = 0xFF; //first two registers as outputs
 	DataDirControl = 0xE0; //pd5,pd6,pd7 as outputs 
 	ControlRegisters = 0; //setting control to low
-	DDRB = 1<<DDRB0;
-	
+	DDRB = 0b0000011;
+
 	/* Send initialization commands to setup LCD */
 	init_lcd();
 	_delay_ms(100);
-	
+
 	/* Send initialization commands to setup USART communication */
 	USART_Init();
-	
-	sei(); //enable global interrupts, this call is defined inside avr/interrupt.h file
+
+	cli(); 
 	
 	while(1){
-		USART_Receive();
-		if(stop_flag) break;
+		int count = 0;
+		
+		for (int i=0;i<7;i++) {
+			ch = USART_Receive();
+				
+			if (ch == gpgga[i]) {
+				//sendCharacter(ch);
+				test_buffer[count] = ch;
+				count++;
+			}
+			else {	
+				count = 0;
+				break; //break out of for loop
+			}
+		}
+		if (count == 7){
+			while(USART_Receive()!= ',');
+			comma = 0;
+			while(comma!=2) {
+				ch = USART_Receive();
+				if(ch != ',' && comma < 2) {
+					latitude[lat_post] = ch;
+					lat_post++;
+				}
+				if (ch == ',' && comma != 2){
+					//latitude[lat_post] = ;
+					//lat_post++;
+					comma++;
+				}
+			}
+		
+			while (comma >= 2 && comma <4){
+				ch = USART_Receive();
+				if(ch != ',' && comma < 4) {
+					longitude[long_post] = ch;
+					long_post++;
+				}
+				if (ch == ',' && comma != 4){
+					comma++;
+				}
+			}
+			
+			latitude[lat_post] = '\0';
+			longitude[long_post] = '\0';
+			lat_post = 0;
+			long_post = 0;
+		}
+		sendString("Lat:");
+		_delay_ms(2);
+		sendString(latitude);
+		_delay_ms(2);
+		sendString("  ");
+		_delay_ms(2);
+		sendString("Long:");
+		_delay_ms(2);
+		sendString(longitude);
+		_delay_ms(1200);
+		//Commands(1);
+		//_delay_ms(16);
+		Commands(2);
+		_delay_ms(16);
 	}
-	
 
-	
-	return 0;
 }
-
-
-/* INTERRUPTS TO BE USED FOR THE PROGRAM */
-ISR(USART0_UDRE_vect){ //might get rid of this interrupt
-	
-	USART_Receive();
-	
-}
-
-ISR(USART0_RX_vect){
-	
-	data_buffer[read_position] = UDR0;
-	read_position++;
-
-	if(read_position >= BUFFER_SIZE){
-		read_position = 0;
-		sendString(data_buffer);
-		stop_flag = true;
-		USART_Flush();
-	}
-}
-/*--------------------------------------*/
-
 
 /* FUNCTION TO INTERFACE RX COM. WITH THE GPS */
 void USART_Init(void){
@@ -102,36 +143,17 @@ void USART_Init(void){
 	UBRR0H = (unsigned char)(UBRR0_VALUE>>8);
 	UBRR0L = (unsigned char)UBRR0_VALUE;
 	/* Enable receiver and transmitter */
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
-	/* Set frame format: 8 data, 1 stop bit */
-	UCSR0C = (3<<UCSZ00);
+	UCSR0B = (1<<RXEN0);//|(1<<RXCIE0);
+	/* Set frame format: 8 data bit, 1 stop bit */
+	UCSR0C = (3<<UCSZ00);//|(1<<USBS0);
 }
 
-unsigned char USART_Receive(void)
-{
+unsigned char USART_Receive(void) {
 	/* Wait for data to be received */
 	while (!(UCSR0A & (1<<RXC0)));
 	/* Get and return received data from buffer */
 	return UDR0;
 }
-
-void USART_Transmit(unsigned char data){
-	DDRB = 0x01;
-	PORTB = 1<<PORTB0;
-	/* Wait for empty transmit buffer */
-	while (!( UCSR0A & (1<<UDRE0))){
-		PORTB |= (~1<<PORTB0);
-	}
-	/* Put data into buffer, sends the data */
-	UDR0 = data;
-}
-
-void USART_Flush(void){
-	unsigned char dummy;
-	while (UCSR0A & (1<<RXC0)) dummy = UDR0;
-}
-/*-------------------------------------------*/
-
 
 /* BELOW ARE FUNCTIONS WRITTEN FOR THE LCD DISPLAY */
 void init_lcd(void){
@@ -146,28 +168,29 @@ void Commands(unsigned char command){
 	DataRegisters = command;
 	ControlRegisters &= ~((1<<rs) | (1<<rw)); //read write and register select are low
 	ControlRegisters |= 1<<en; //execute commands stored in data register
-	asm volatile("nop");
-	asm volatile("nop");
+	_delay_us(5);
 	ControlRegisters &= ~(1<<en);
+	_delay_us(5);
 }
 
-void sendCharacter(char character){
+void sendCharacter(unsigned char character){
 	DataRegisters = character;
 	ControlRegisters &= ~(1<<rw); //turn off RW (write mode)
 	ControlRegisters |= (1<<rs); //turn on RS (character display mode)
 	ControlRegisters |= (1<<en);
-	asm volatile("nop");
-	asm volatile("nop");
+	_delay_us(5);
 	ControlRegisters &= ~1<<en;
+	_delay_us(5);
 }
 
-void sendString(char *str){
+void sendString(unsigned char *str){
 	
 	while(*str != '\0'){
 		sendCharacter(*str);
 		_delay_ms(2);
 		str++;
 	}
+	return;
 }
 /*-------------------------------------------------*/
 
