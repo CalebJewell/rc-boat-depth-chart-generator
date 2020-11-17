4ab12a9da5cf0b12a4037c7ef5f2dfc40ad31c00
Caleb Stephen Jewell <caleb.jewell@maine.edu>
2020-11-17 11:21:53 -0500

    Gave credit to code and resources used for fat file system

--- atmel-dev/gps-sd-fatfs/gps-sd-fatfs/diskio.c
+++ atmel-dev/gps-sd-fatfs/gps-sd-fatfs/diskio.c
@@ -1,3 +1,20 @@
+/* disk.c and disk.h from https://github.com/jambu-aj/FatFS/blob/master/GccApplication1_FatFS_SDCard/GccApplication1_FatFS_SDCard/diskio.c using FatFs open-source library. */
+/*----------------------------------------------------------------------------/
+/  FatFs - FAT file system module  R0.09a                 (C)ChaN, 2012
+/-----------------------------------------------------------------------------/
+/ FatFs module is a generic FAT file system module for small embedded systems.
+/ This is a free software that opened for education, research and commercial
+/ developments under license policy of following terms.
+/
+/  Copyright (C) 2012, ChaN, all right reserved.
+/
+/ * The FatFs module is a free software and there is NO WARRANTY.
+/ * No restriction on use. You can use, modify and redistribute it for
+/   personal, non-profit or commercial products UNDER YOUR RESPONSIBILITY.
+/ * Redistributions of source code must retain the above copyright notice.
+/
+/-----------------------------------------------------------------------------/
+
 /*-----------------------------------------------------------------------*/
 /* MMCv3/SDv1/SDv2 (in SPI mode) control module  (C)ChaN, 2010           */
 /*-----------------------------------------------------------------------*/
